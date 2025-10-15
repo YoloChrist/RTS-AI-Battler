9c999c4b4874ce274b387dc383750d87d8eda560
YoloChrist <57275834+YoloChrist@users.noreply.github.com>
2025-10-15 15:08:22 +0100

    Changed logic from controller to script
     - UnitAIController.cs now handles following and attacking enemies.
     - AttackController.cs still handles selecting enemies
    
    Changed because it was checking every frame.

---
+++ Assets/UnitAIController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitAIController : MonoBehaviour
+{
+    public Transform targetToAttack;
+    
+    public float attackingDistance = 2.0f;
+    public float updateInterval = 0.2f;
+
+    private NavMeshAgent agent;
+    private float lastUpdateTime;
+    private AttackController attackController;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        attackController = GetComponent<AttackController>();
+    }
+
+
+    void Update()
+    {
+        if (attackController == null || attackController.targetToAttack == null)
+            return;
+
+        Transform targetToAttack = attackController.targetToAttack;
+        float distance = Vector3.Distance(transform.position, targetToAttack.position);
+
+        //Update path at intervals
+
+        if (Time.time - lastUpdateTime > updateInterval)
+        {
+            agent.SetDestination(targetToAttack.position);
+            lastUpdateTime = Time.time;
+        }
+
+        // Face target
+        Vector3 lookDir = (targetToAttack.position - transform.position).normalized;
+        lookDir.y = 0;
+        if (lookDir !=  Vector3.zero)
+            transform.rotation = Quaternion.LookRotation(lookDir);
+
+        if (distance < attackingDistance) // attack logic
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void SetTarget(Transform target)
+    {
+        targetToAttack = target;
+    }
+
+    public void ClearTarget()
+    {
+        targetToAttack = null;
+        agent.ResetPath();
+    }
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Enemy") && targetToAttack == null)
+    //    {
+    //        targetToAttack = other.transform;
+    //    }
+    //}
+    //
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    if (other.CompareTag("Enemy") && targetToAttack != null)
+    //        targetToAttack = null;
+    //}
+}
