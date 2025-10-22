90681e4dc9ddad49f5be928d94c45ae271a97411
YoloChrist <57275834+YoloChrist@users.noreply.github.com>
2025-10-22 22:01:07 +0100

    Added unit health bar

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider slider;
+    public void Start()
+    {
+        UnitRuntimeStats unitStats = GetComponentInParent<UnitRuntimeStats>();
+
+        if (unitStats != null)
+        {
+            unitStats.HealthChanged += OnHealthChanged;
+            updateBarValue(unitStats.CurrentHealth, unitStats.MaxHealth);
+        }
+        else
+        {
+            Debug.LogWarning("HealthBar: No UnitRuntimeStats found in parent objects.");
+        }
+    }
+
+    private void OnHealthChanged(int currentHealth)
+    {
+        UnitRuntimeStats unitStats = GetComponentInParent<UnitRuntimeStats>();
+        if (unitStats != null)
+        {
+            updateBarValue(currentHealth, unitStats.MaxHealth);
+            Debug.Log("HealthBar: Updated bar value to " + currentHealth + "/" + unitStats.MaxHealth);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        UnitRuntimeStats unitStats = GetComponentInParent<UnitRuntimeStats>();
+        if (unitStats != null)
+            unitStats.HealthChanged -= OnHealthChanged;
+    }
+
+    public void updateBarValue(float currentHealth, float maxHealth)
+    {
+        float healthPercentage = Mathf.Clamp01(currentHealth / maxHealth);
+        float invertedPercentage = 1f - healthPercentage;
+
+        if (slider != null)
+            slider.value = invertedPercentage;
+        else
+            Debug.LogWarning("HealthBar: No Slider component found.");
+    }
+}
